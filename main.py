2e4f5fbc9ca4c5c18036dc96540fa23a23601c7a
a-powers <awpowers@gmail.com>
2021-02-05 14:30:15 +0000

    update

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ operations = {
 }
 
 
-num1 = int(input("What is the first number"))
-num2 = int(input("What is the second number"))
+num1 = int(input("What is the first number? \n"))
+num2 = int(input("What is the second number? \n"))
 
 for each_key in operations:
     print(each_key)
@@ -31,4 +31,4 @@ operation_symbol = input("Pick an operation from the line above.")
 
 calculation_function = operations[operation_symbol]
 answer = calculation_function(num1, num2)
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+print(f"{num1} {operation_symbol} {num2} = {answer}")   
