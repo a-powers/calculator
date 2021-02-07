50392691f6e589b2b25eea487f107501a431a176
a-powers <awpowers@gmail.com>
2021-02-07 03:17:01 +0000

    update

--- main.py
+++ main.py
@@ -18,21 +18,26 @@ operations = {
     "/": divide,
 }
 
-num1 = int(input("What is the first number? "))
-for symbol in operations:
-    print(symbol)
-continue_calculation = True
 
-while continue_calculation:
-    operation_symbol = input("Pick an operation from the line above.")
-    num2 = int(input("What is the next number? "))
-    calculation_function = operations[operation_symbol]
-    answer = calculation_function(num1, num2)
+def calculator():
 
-    print(f"{num1} {operation_symbol} {num2} = {answer}")
-    
-    if input(f"Type 'y' to continue calculating with {answer} or 'n' to exit.") == 'y':
-        num1 = answer
-    else:
-        continue_calculation = False
+    num1 = int(input("What is the first number? "))
+    for symbol in operations:
+        print(symbol)
+    continue_calculation = True
 
+    while continue_calculation:
+        operation_symbol = input("Pick an operation from the line above.")
+        num2 = int(input("What is the next number? "))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
+
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        if input(f"Type 'y' to continue calculating with {answer} or 'n' to exit.") == 'y':
+            num1 = answer
+        else:
+            continue_calculation = False
+            calculator()
+
+calculator()
