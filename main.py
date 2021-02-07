f67e3965044f4e4ef0ba3b61cc3be0d4373e6d0a
a-powers <awpowers@gmail.com>
2021-02-07 01:37:42 +0000

    update

---
+++ main.py
@@ -0,0 +1,38 @@
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply, 
+    "/": divide,
+}
+
+num1 = int(input("What is the first number? "))
+for symbol in operations:
+    print(symbol)
+continue_calculation = True
+
+while continue_calculation:
+    operation_symbol = input("Pick an operation from the line above.")
+    num2 = int(input("What is the next number? "))
+    calculation_function = operations[operation_symbol]
+    answer = calculation_function(num1, num2)
+
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+    
+    if input(f"Type 'y' to continue calculating with {answer} or 'n' to exit.") == 'y':
+        num1 = answer
+    else:
+        continue_calculation = False
+
